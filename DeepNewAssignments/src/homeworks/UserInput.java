6d75471ab6926b2e8c4dac7f6984b41faa961d1b
yanna <yanna@LAPTOP-GE0MH9LO.home>
2018-10-24 23:03:23 -0500

    Day10 Assignment

---
+++ DeepNewAssignments/src/homeworks/UserInput.java
@@ -0,0 +1,30 @@
+package homeworks;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class UserInputTest {
+
+	public static void main(String[] args) {
+
+		int userInput;
+
+		Scanner scan = new Scanner(System.in);
+
+		try {
+			System.out.println("Please enter a number");
+			userInput = scan.nextInt();
+			System.out.println("The number entered by you is : " + userInput);
+		} catch (InputMismatchException e) {
+			System.out.println("Try again ,the number you entered is not an integer.Please try again.");
+		}
+
+	}
+
+}
+
+
+/* Assignment 2
+   openBrowser() & closeBrowser() methods can be placed in a separate class & 
+   make the remaining classes as child class */
+ 
